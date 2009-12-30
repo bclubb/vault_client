f6dd3b491a8c4a4970db0d5e286f6e5c76103628
Brian Clubb <bclubb@gmail.com>
2009-12-30 11:27:23 -0600

    initial version

---
+++ MyDocument.h
@@ -0,0 +1,15 @@
+//
+//  MyDocument.h
+//  vault_client
+//
+//  Created by Brian Clubb on 12/30/09.
+//  Copyright 2009 MTM Recognition. All rights reserved.
+//
+
+
+#import <Cocoa/Cocoa.h>
+
+@interface MyDocument : NSDocument
+{
+}
+@end
---
+++ main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  vault_client
+//
+//  Created by Brian Clubb on 12/30/09.
+//  Copyright 2009 MTM Recognition. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **) argv);
+}
